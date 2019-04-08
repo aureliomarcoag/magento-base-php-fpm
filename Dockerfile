36f0a0220906e6c8e4abc13a0dfe8e6551de6be0
Marco Godinho <aurelio.marco.ag@gmail.com>
2019-04-07 22:44:58 -0300

    Turn Magento into a service

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,35 @@
 FROM php:7.2-fpm-stretch
 
-RUN apt-get update \
+# git and procps are required to run setup:dev:tests
+
+RUN apt-get update --no-install-recommends \
     && apt-get install -y \
     libpng-dev \
     libicu-dev \
     libxmlrpc-core-c3-dev \
     libxslt1-dev \
     libfreetype6-dev \
-    libjpeg62-turbo-dev
-
-RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
-    && docker-php-ext-install bcmath gd intl pdo_mysql soap hash opcache xsl zip
-
-RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
+    libjpeg62-turbo-dev \
+    git \
+    procps \
+    nginx \
+    && rm -rf /var/lib/apt/lists/* \
+    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-install bcmath gd intl pdo_mysql soap hash opcache xsl zip \
+    && php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
     && php composer-setup.php --install-dir=/usr/local/bin --filename=composer \
-    && php -r "unlink('composer-setup.php');"
+    && php -r "unlink('composer-setup.php');" \
+    && curl -s http://gordalina.github.io/cachetool/downloads/cachetool.phar -o /usr/local/bin/cachetool && chmod +x /usr/local/bin/cachetool \
+    && mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini" \
+    && find "$PHP_INI_DIR" -name php.ini -exec sed -i 's/memory_limit.*/memory_limit = -1/g' {} \; \
+    && rm /usr/local/etc/php-fpm.d/* -f \
+    && rm -Rf /var/www \
+    && groupadd web -g 1212 \
+    && useradd -m -u 1212 -g web -s /bin/bash magento
 
-RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
-RUN find "$PHP_INI_DIR" -name php.ini -exec sed -i 's/memory_limit.*/memory_limit = -1/g' {} \;
+COPY nginx/* /etc/nginx/
+COPY fpm.conf /usr/local/etc/php-fpm.d/
+COPY magento /var/www
+COPY entrypoint.sh /entrypoint.sh
 
-RUN groupadd web -g 1212
-RUN useradd -m -u 1212 -g web -s /bin/bash magento
-USER magento
+CMD ["bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# --http-cache-hosts is not a valid argument for the entrypoint
+# because in case it had already been set, the new value might overwrite correct previous values
+# Optional arguments and their default values:
+# AMQP_VIRTUALHOST: /
+# AMQP_SSL: ""
+# AMQP_SSL_OPTIONS: ""
+# DB_MODEL: mysql4
+# DB_PREFIX: ""
+# DB_ENGINE: innodb
+# SESSION_SAVE_REDIS_PASSWORD: ""
+# SESSION_SAVE_REDIS_TIMEOUT: 10
+# SESSION_SAVE_REDIS_PERSISTENT_ID: ""
+# SESSION_SAVE_REDIS_DB: 1
+# SESSION_SAVE_REDIS_COMPRESSION_LIB: gzip
+# SESSION_SAVE_REDIS_LOG_LEVEL: 0
+# SESSION_SAVE_REDIS_MAX_CONCURRENCY: 12
+# CACHE_BACKEND: Cm_Cache_Backend_Redis
+# PAGE_CACHE: Cm_Cache_Backend_Redis
+php /var/www/bin/magento setup:config:set \
+    --backend-frontname="$BACKEND_FRONTNAME" \
+    --key="${KEY}" \
+    --amqp-host="$AMQP_HOST" \
+    --amqp-port="$AMQP_PORT" \
+    --amqp-user="$AMQP_USER" \
+    --amqp-password="$AMQP_PASSWORD" \
+    --amqp-virtualhost="${AMQP_VIRTUALHOST:-/}" \
+    --amqp-ssl="${AMQP_SSL:-}" \
+    --amqp-ssl-options="${AMQP_SSL_OPTIONS:-}" \
+    --db-host="$DB_HOST" \
+    --db-name="$DB_NAME" \
+    --db-user="$DB_USER" \
+    --db-engine="${DB_ENGINE:-innodb}" \
+    --db-password="$DB_PASSWORD" \
+    --db-prefix="${DB_PREFIX:-}" \
+    --db-model="$DB_MODEL:-mysql4" \
+    --session-save="${SESSION_SAVE}"
+    --session-save-redis-host="${SESSION_SAVE_REDIS_HOST}"
+    --session-save-redis-port="${SESSION_SAVE_REDIS_PORT}"
+    --session-save-redis-timeout="${SESSION_SAVE_REDIS_TIMEOUT:-10}"
+    --session-save-redis-persistent-id="${SESSION_SAVE_REDIS_PERSISTENT_ID:-}"
+    --session-save-redis-db="${SESSION_SAVE_REDIS_DB:-1}"
+    --session-save-redis-compression-lib="${SESSION_SAVE_REDIS_COMPRESSION_LIB:-gzip}"
+    --session-save-redis-log-level="${SESSION_SAVE_REDIS_LOG_LEVEL:-0}"
+    --session-save-redis-max-concurrency="${SESSION_SAVE_REDIS_MAX_CONCURRENCY:-12}"
+    --cache-backend="${CACHE_BACKEND:-Cm_Cache_Backend_Redis}"
+    --cache-backend-redis-server="${CACHE_BACKEND_REDIS_SERVER}"
+    --cache-backend-redis-db="${CACHE_BACKEND_REDIS_DB}"
+    --cache-backend-redis-port="${CACHE_BACKEND_REDIS_PORT}"
+    --page-cache="${PAGE_CACHE:-Cm_Cache_Backend_Redis}"
+    --page-cache-redis-server="${PAGE_CACHE_REDIS_SERVER}"
+    --page-cache-redis-db="${PAGE_CACHE_REDIS_DB}"
+    --page-cache-redis-port="${PAGE_CACHE_REDIS_PORT}"
+    --page-cache-redis-compress-data="${PAGE_CACHE_REDIS_COMPRESS_DATA}"
+
+php /var/www/bin/magento deploy:mode:set --skip-compilation "$MAGE_MODE"
+
+# If either PHP or Nginx dies, the container exits
+php-fpm -F &
+fpm_pid=$!
+nginx -g "daemon off;" &
+nginx_pid=$!
+
+while [ -d /proc/$fpm_pid ] && [ -d /proc/$nginx_pid ] ; do
+    sleep 1
+done
